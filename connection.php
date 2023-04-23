9a390cfe6906afb4ce853dae53c9a07b32b69cec
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-23 23:38:56 +0800

    Changed Database

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "u235219407_caywaiitech";
-$password = "Caywaiitech2023";
-$database = "u235219407_caywaiitech";
+$username = "u235219407_caywaii2023";
+$password = "Caywaii2023";
+$database = "u235219407_caywaii";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
